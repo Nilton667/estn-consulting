f78ec2b63f64cbd3866713851f12d1f688955276
nilton667 <nilton667@gmail.com>
2023-01-03 16:15:54 +0100

    0.2

---
+++ pg/certificacoes.php
@@ -0,0 +1,22 @@
+<div class="header-container-wallpaper d-flex" 
+style="background-color: #6d8be3; background-image: url(publico/img/crossword.png);">
+    
+    <div class="container h-100">
+        <div class="row">
+            <div class="col-12">
+                <h2 style="text-shadow: 4px 3px 5px rgba(0,0,0,0.3); word-wrap: break-word;" class="white-text m-0">Certificações</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<br>
+
+<div>
+    <div class="container">
+        <div class="row m-0 justify-content-center">
+            
+        </div>
+    </div>
+    <br>
+</div>
---
+++ pg/parceiros.php
@@ -0,0 +1,33 @@
+<style>
+    .logo-parca{
+        opacity: 0.5;
+        transition: all 0.3s;
+    }
+    .logo-parca:hover{
+        opacity: 1;
+    }
+</style>
+<div class="header-container-wallpaper d-flex" 
+style="background-color: #6d8be3; background-image: url(publico/img/crossword.png);">
+    
+    <div class="container h-100">
+        <div class="row">
+            <div class="col-12">
+                <h2 style="text-shadow: 4px 3px 5px rgba(0,0,0,0.3); word-wrap: break-word;" class="white-text m-0">Parceiros</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<br>
+
+<div>
+    <div class="container">
+        <div class="row m-0 justify-content-center">
+            <div class="col-12 col-sm-6 col-md-3">
+                <img class="img-fluid logo-parca" src="publico/img/parceiros/1.jpeg" alt="Logo">
+            </div>
+        </div>
+    </div>
+    <br>
+</div>
